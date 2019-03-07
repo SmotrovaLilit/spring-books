9b4c515d2281d8d867bfca9eb4b2be1ab4efa478
SmotrovaLilit <lilit2990@gmail.com>
2019-03-07 04:05:37 +0300

    Домашнее задание к уроку 14 Spring MVC View - тесты - хранить дату создания чтобы можно было сортировать - редирект с ошибкаим при редактировании - валидация для года

--- src/main/java/ru/lilitweb/books/BooksApplication.java
+++ src/main/java/ru/lilitweb/books/BooksApplication.java
@@ -2,14 +2,13 @@ package ru.lilitweb.books;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.function.Function;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableMongoAuditing
 public class BooksApplication {
     public static void main(String[] args) {
         SpringApplication.run(BooksApplication.class, args);
--- src/main/java/ru/lilitweb/books/web/form/BookForm.java
+++ src/main/java/ru/lilitweb/books/web/form/BookForm.java
@@ -1,13 +1,8 @@
 package ru.lilitweb.books.web.form;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.lang.NonNull;
+import lombok.*;
+import ru.lilitweb.books.web.validation.YearConstraint;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,23 +13,20 @@ import java.util.List;
 @AllArgsConstructor
 public class BookForm {
 
-    @NotBlank(message = "{bookForm.error.title.empty}")
     @Size(min = 3, max=2024, message = "{bookForm.error.title.incorrect}")
     private String title;
 
-    @NonNull
-    @NotBlank(message = "{bookForm.error.author.empty}")
     @Size(min = 3, max=2024, message = "{bookForm.error.author.incorrect}")
     private String author;
 
     @NonNull
-    @Min(value = 1900, message = "{bookForm.error.year.incorrect}")
+    @YearConstraint(message = "{bookForm.error.year.incorrect}")
     private int year;
 
     @NonNull
-    @NotBlank(message = "{bookForm.error.description.empty}")
     @Size(min = 3, message = "{bookForm.error.description.incorrect}")
     private String description;
 
     private List<String> genres = new ArrayList<>();
 }
+
